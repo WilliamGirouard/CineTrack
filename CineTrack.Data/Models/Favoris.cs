aec5abf536fdb9e8132db0cd26aaa75bb38d830e
amineelghazi <amineelghazi100@hotmail.com>
2026-03-25 16:47:55 -0400

    Ajout de DbSet<Favoris> dans le Context, ajout de l'attribut ImageUrl

--- CineTrack.Data/Models/Favoris.cs
+++ CineTrack.Data/Models/Favoris.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,21 @@ namespace CineTrack.Data.Models
 {
     public class Favoris
     {
+
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public int UtilisateurId { get; set; }
+
+        [Required]
         public int AnimeId { get; set; }
 
-        public required string TitreAnime { get; set; }
+        [Required]
+        public string? ImageUrl { get; set; }
+
+        [Required]
+        public string? TitreAnime { get; set; }
 
     }
 }
