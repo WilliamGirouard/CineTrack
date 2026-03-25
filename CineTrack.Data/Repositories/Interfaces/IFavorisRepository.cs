5dbbd3060bd5524525600bdfbb4748f566623155
amineelghazi <amineelghazi100@hotmail.com>
2026-03-25 02:33:39 -0400

    Ajout de FavorisRepository et IFavorisRepository

---
+++ CineTrack.Data/Repositories/Interfaces/IFavorisRepository.cs
@@ -0,0 +1,16 @@
+﻿using CineTrack.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineTrack.Data.Repositories.Interfaces
+{
+    public interface IFavorisRepository
+    {
+        List<Favoris> GetFavorisByUserId(int userId);
+        void AddFavoris(Favoris favoris);
+        void RemoveFavoris(int Id);
+    }
+}
