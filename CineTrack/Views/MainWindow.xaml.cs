623113cd764948c7c9ba0b5494f3039df8f1309c
yassine ani <yanidoatio@gmail.com>
2026-03-25 21:05:44 -0400

    Ajout navigation singIn/SignUp

--- CineTrack/Views/MainWindow.xaml.cs
+++ CineTrack/Views/MainWindow.xaml.cs
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
+using CineTrack.Data.Services.Interfaces;
 using CineTrack.Services;
 using CineTrack.ViewModels.Auth;
-=======
-using CineTrack.Data.Services.Interfaces;
->>>>>>> 703482772dbb4fce2ccf0f62bba2ef19b82c61e4
 using CineTrack.Views.Auth;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -20,19 +17,20 @@ namespace CineTrack
         public MainWindow(IUtilisateurService utilisateurService, IAnimeService animeService, IUtilisateurAnimeService utilisateurAnimeService)
         {
             InitializeComponent();
-
             _utilisateurService = utilisateurService;
             _animeService = animeService;
             _utilisateurAnimeService = utilisateurAnimeService;
 
             // Show SignUpView for navigation test
             var signUpView = App.ServiceProvider.GetRequiredService<SignUpView>();
+            var signInView = App.ServiceProvider.GetService<SignInView>();
             MainFrame.Navigate(signUpView);
 
+            // Navigation entre SignIn et SignUp
             var nav = (NavigationService)App.ServiceProvider.GetRequiredService<INavigationService>();
             nav.PropertyChanged += (s, e) =>
             {
-                if (nav.CurrentView is SignInViewModel) //logique if pour inverse les page de sign IN and UP 
+                if (nav.CurrentView is SignInViewModel) //logique if pour inverser les pages SignIn et SignUp
                     MainFrame.Navigate(signInView);
                 else if (nav.CurrentView is SignUpViewModel)
                     MainFrame.Navigate(signUpView);
