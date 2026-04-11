0c1c13c9a14bfae2d1a9820d22e5c41a9a1b512d
Khaled-AbHe <6320936@cegepmv.ca>
2026-04-11 19:54:30 -0400

    Added carousels in main page based on genres. Also made AnimeCards that is a hybrid of Jikan's animes and what we will be tracking (like notes)

--- CineTrack/App.xaml.cs
+++ CineTrack/App.xaml.cs
@@ -16,6 +16,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.IO;
 using System.Windows;
+using CineTrack.Services.Jikan;
 
 namespace CineTrack
 {
@@ -50,13 +51,13 @@ namespace CineTrack
             services.AddScoped<IUtilisateurAnimeService, UtilisateurAnimeService>();
 
             services.AddScoped<IAnimeService, AnimeService>();
-            services.AddScoped<IAnimeApiService, AnimeApiService>();
             services.AddScoped<IAnimeRepository, AnimeRepository>();
 
             services.AddScoped<IFavorisRepository, FavorisRepository>();
 
             // ── Navigation (Singleton) ─────────────────────────────────────────
             services.AddSingleton<INavigationService, NavigationService>();
+            services.AddSingleton<IJikanService, JikanService>();
 
             // ── ViewModels (Transient) ─────────────────────────────────────────
             services.AddTransient<SignInViewModel>();
---
+++ CineTrack/Services/Jikan/IJikanService.cs
@@ -0,0 +1,9 @@
+﻿using JikanDotNet;
+
+namespace CineTrack.Services.Jikan;
+
+public interface IJikanService
+{
+    Task<AnimeFull> GetAnimeByIdAsync(int id);
+    Task<ICollection<Anime>> GetAnimesByGenreAsync(int genreId, int page = 1);
+}
